72991138c5203e7e3e72d8bffa4d40ef7e1973ba
SergeyBel <belovseregas@gmail.com>
2019-05-01 16:23:16 +0300

    random generator encoder

--- tests/Random/RandomGeneratorTest.php
+++ tests/Random/RandomGeneratorTest.php
@@ -2,7 +2,7 @@
 
 namespace NiceCrypto\Tests\Cipher;
 
-use NiceCrypto\Random\RandomGenerator;
+use NiceCrypto\Random\RamdomBytesGenerator;
 use PHPUnit\Framework\TestCase;
 
 class RandomGeneratorTest extends TestCase
@@ -10,7 +10,7 @@ class RandomGeneratorTest extends TestCase
     public function testKeyGenerate()
     {
         $bytesLength = 128;
-        $g = new RandomGenerator();
+        $g = new RamdomBytesGenerator();
         $key = $g->generateRandomBytes($bytesLength);
         $this->assertEquals(256, strlen($key));
     }
