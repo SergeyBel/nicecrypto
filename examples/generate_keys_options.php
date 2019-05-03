be20633f7b6f8543678356b43b82971228791658
SergeyBel <belovseregas@gmail.com>
2019-05-03 20:35:01 +0300

    add geretae keys example

---
+++ examples/generate_keys_options.php
@@ -0,0 +1,17 @@
+<?php
+require_once '../vendor/autoload.php';
+
+use \NiceCrypto\Certificate\GenerateOptions;
+use \NiceCrypto\Certificate\Pem\PemGenerator;
+
+
+$bitsLength = 2048;
+$passphrase = 'password';
+$options = new GenerateOptions();
+$options->setBits($bitsLength);
+$generator = new PemGenerator();
+
+$privateKey = $generator->generatePrivateKey($passphrase, $options);
+$publicKey = $generator->generatePublicKey($privateKey);
+file_put_contents('private.pem', $privateKey->toString());
+file_put_contents('public.pem', $publicKey->toString());
